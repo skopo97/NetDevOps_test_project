d9d8f228e45b65c66ff9c6e03abfe23b0ea8eb82
Sami Koponen <sami.t.koponen@icloud.com>
2026-06-17 11:48:41 +0300

    modified test_conn.py script

--- pyATS/scripts/test_conn.py
+++ pyATS/scripts/test_conn.py
@@ -2,16 +2,15 @@
 from pyats.topology import loader
 
 # Define the testbed for pyATS to use
-testbed = loader.load('testbed.yaml')
+testbed = loader.load("../testbed.yaml")
 
 # Define the device that will be connected to in this script
-device = testbed.devices['R1']
+device = testbed.devices["R1"]
 
 # Try to connect, if successful print "connected" and execute "show version"
 try:
     device.connect(log_stdout=False)
     print("Connected")
-    print(device.execute('show ip int br'))
 
 # Print exception message if connection fails
 except Exception as e:
